1b78cbe01da44f332dcf8dea64cb92d45364835b
tz <17604206400@163.com>
2020-08-31 12:30:44 +0800

    第一次提交

---
+++ gittest/src/com/tz/gittest/testgit.java
@@ -0,0 +1,12 @@
+package com.tz.gittest;
+
+public class testgit {
+
+    public static void main(String[] args) {
+        System.out.println("hhhhhhhh");
+        System.out.println("hhhhhhhh");
+        System.out.println("hhhhhhhh");
+        System.out.println("hhhhhhhh");
+        System.out.println("hhhhhhhh");
+    }
+}
